a74f1454977a8bfd8eaba66f4d7f9d0372d7803d
Akash Srivastava <akashniki@gmail.com>
2026-06-21 10:12:48 +0200

    common: perf: Allow workloadclassifier to start background services
    * WLCReceiver spawns a WLCServiceStart thread from onReceive(), losing the transient background-start privilege by the time the thread calls startService(). This causes a BackgroundServiceStartNotAllowedException crash on every APP_LAUNCH broadcast when the app is idle.
    * Add a sysconfig allowlist entry to exempt com.qualcomm.qti.workloadclassifier from the device idle background service start restriction.
    * Fixes the following crash:
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: FATAL EXCEPTION: WLCServiceStart
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: Process: com.qualcomm.qti.workloadclassifier, PID: 20994
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: android.app.BackgroundServiceStartNotAllowedException: Not allowed to start service Intent { act=com.qualcomm.qti.workloadclassifier.APP_LAUNCH flg=0x30 xflg=0x4 pkg=com.qualcomm.qti.workloadclassifier cmp=com.qualcomm.qti.workloadclassifier/.WLCService (has extras) }: app is in background uid UidRecord{f115b9a u0a399 CEM  bg:+20ms idle change:cached|procstate|procadj procs:0 seq(22250,22250)} caps=--------
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: 	at android.app.ContextImpl.startServiceCommon(ContextImpl.java:2048)
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: 	at android.app.ContextImpl.startService(ContextImpl.java:2003)
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: 	at android.content.ContextWrapper.startService(ContextWrapper.java:855)
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: 	at android.content.ContextWrapper.startService(ContextWrapper.java:855)
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: 	at com.qualcomm.qti.workloadclassifier.WLCReceiver$StartRunnable.run(WLCReceiver.java:60)
    06-18 11:48:47.910 20994 21013 E AndroidRuntime: 	at java.lang.Thread.run(Thread.java:1119)
    
    Change-Id: I8514b31a316d0414bc71a809090d1a4ac2481b08

--- system/perf/qti-perf.mk
+++ system/perf/qti-perf.mk
@@ -8,5 +8,9 @@ PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
 
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(QCOM_COMMON_PATH)/system/perf/workloadclassifier.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/workloadclassifier.xml
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/qcom/common/system/perf/perf-vendor.mk)
